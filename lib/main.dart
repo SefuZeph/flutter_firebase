a49722e53312ba58b10de8563dd3d7a872093cc2
sefu <zephaniahsefuoduori@gmail.com>
2021-02-15 07:43:59 +0300

    Check if firebase has been initialized

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("FlutterFire"),
+        ),
+        body: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<FirebaseApp> _initialization;
+
+  @override
+  void initState() {
+    super.initState();
+    _initialization = Firebase.initializeApp();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return FutureBuilder<FirebaseApp>(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          Text("Some error here");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Text("Connected");
+        }
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
